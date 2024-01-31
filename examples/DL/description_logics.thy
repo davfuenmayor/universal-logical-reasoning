87d9cc0569bfd669ab4725a69b01569f6af19df8
David Fuenmayor <davfuenmayor@gmail.com>
2024-01-31 18:22:11 +0100

    update example

--- examples/DL/description_logics.thy
+++ examples/DL/description_logics.thy
@@ -9,7 +9,8 @@ begin
 
 section \<open>Description Logic Experiments\<close>
 
-subsection \<open>Encoding ALC (+ nominals and number restrictions)\<close>
+
+subsection \<open>Encoding basic connectives\<close>
 
 abbreviation dl_conj (infixr "\<^bold>\<sqinter>" 71)
   where "A \<^bold>\<sqinter> B \<equiv> A \<inter> B"
@@ -23,33 +24,42 @@ abbreviation dl_top ("\<^bold>\<top>")
 abbreviation dl_bot ("\<^bold>\<bottom>")
   where "\<^bold>\<bottom> \<equiv> \<emptyset>"
 
-(*Introduce convenient postfix-superscript notation for the complement (negation) of a relation (role).*)
-notation (input) complR ("(_\<^sup>-)" [1000]1000)
-
 abbreviation dl_ex ("\<^bold>\<exists>_\<sqdot>_")   (*notation warning: the thick dot is written: \sqdot *)
   where "\<^bold>\<exists>r\<sqdot>C \<equiv> rPreimage r C"
 abbreviation dl_all ("\<^bold>\<forall>_\<sqdot>_")
   where "\<^bold>\<forall>r\<sqdot>C \<equiv> rDualPreimage r C"
 
+(*recalling definitions*)
+lemma "\<^bold>\<exists>r\<sqdot>C = (\<lambda>a. \<exists>b. r a b \<and> C b)" by (simp add: rPreimage_def)
+lemma "\<^bold>\<forall>r\<sqdot>C = (\<lambda>a. \<forall>b. r a b \<rightarrow> C b)" by (simp add: rDualPreimage_def)
+
 (*(Qualified) number restrictions \<le>\<^sub>nr (\<le>\<^sub>nr.C) can also be introduced (see below for an example) *)
 
-abbreviation dl_subs::"ERel(Set('i))" (infix "\<^bold>\<sqsubseteq>" 65) 
-  where "A \<^bold>\<sqsubseteq> B \<equiv> A \<subseteq> B"
 
-abbreviation dl_equiv::"ERel(Set('i))" (infix "\<^bold>\<equiv>" 65)
-  where "A \<^bold>\<equiv> B \<equiv> A \<^bold>\<sqsubseteq> B \<and> B \<^bold>\<sqsubseteq> A"
+subsection \<open>Encoding assertions\<close>
 
-(*Note that by extensionality:*)
-lemma dl_equiv_char: "A \<^bold>\<equiv> B = (A = B)" unfolding subset_def by auto
-
+(*Individuals being instances of concepts*)
 abbreviation(input) con_inst::"'i \<Rightarrow> Set('i) \<Rightarrow> bool" (infix ":" 65)
   where "a : A \<equiv> A a"
 
-abbreviation(input) rel_inst::"'i \<Rightarrow> 'i \<Rightarrow> ERel('i) \<Rightarrow> bool" ("\<lparr>_ , _\<rparr> : _")
-  where "\<lparr>a, b\<rparr> : r \<equiv> r a b"
+(*Pairs of individuals being related by a relation/role*)
+abbreviation(input) rel_inst::"'i \<Rightarrow> 'i \<Rightarrow> ERel('i) \<Rightarrow> bool" ("'(_ , _') : _")
+  where "(a, b) : r \<equiv> r a b"
+
+(*Concept inclusion*)
+abbreviation dl_subs::"ERel(Set('i))" (infix "\<^bold>\<sqsubseteq>" 65) 
+  where "A \<^bold>\<sqsubseteq> B \<equiv> A \<subseteq> B"
+
+(*Concept equivalence*)
+abbreviation dl_equiv::"ERel(Set('i))" (infix "\<^bold>\<equiv>" 65)
+  where "A \<^bold>\<equiv> B \<equiv> A \<^bold>\<sqsubseteq> B \<and> B \<^bold>\<sqsubseteq> A"
+
+(*Note that by extensionality, concept equivalence correspond to equality*)
+lemma dl_equiv_char: "A \<^bold>\<equiv> B = (A = B)" unfolding subset_def by auto
 
 
-subsection \<open>Vocabulary\<close>
+
+subsection \<open>Example 1 - Knowledge Base\<close>
 
 typedecl i (*type for individuals*)
 type_synonym c = "Set(i)" (*type for concepts*)
@@ -67,28 +77,26 @@ consts parentOf::r    brotherOf::r    childOf::r     uncleOf::r
 consts Alice::i    Bob::i    GeorgeOrwell::i    AnimalFarm::i    Bihar::i    India::i
 
 
-subsection \<open>Knowledge Base Example\<close>
-
-(*TBox*)
+(*TBox ('terminological box' or 'ontology')*)
 axiomatization where
   T1: "Writer \<^bold>\<equiv> (Person \<^bold>\<sqinter> \<^bold>\<exists>author\<sqdot>Book)" and
   T2: "Novel \<^bold>\<sqsubseteq> Book"
 (* .... *)
 
-(*ABox*)
+(*ABox ('assertional box')*)
 axiomatization where
   A1: "GeorgeOrwell : Person" and
   A2: "AnimalFarm : Novel" and
-  A3: "\<lparr>GeorgeOrwell, AnimalFarm\<rparr> : author" and
-  A4: "\<lparr>GeorgeOrwell, Bihar\<rparr> : bornIn" and
-  A5: "\<lparr>Bihar, India\<rparr> : cityOf"
+  A3: "(GeorgeOrwell, AnimalFarm) : author" and
+  A4: "(GeorgeOrwell, Bihar) : bornIn" and
+  A5: "(Bihar, India) : cityOf"
 (* .... *)
 
-(* GeorgeOrwell is a writer*)
+(* Given the background knowledge, we can infer that GeorgeOrwell is a writer*)
 lemma "GeorgeOrwell : Writer"
   by (metis (full_types) A1 A2 A3 T1 T2 inter_def rPreimage_def subset_def)
 
-(* GeorgeOrwell is (supposed to be) European*)
+(* Can we infer that GeorgeOrwell is European?*)
 lemma "GeorgeOrwell : European" nitpick oops (*countermodel found: background knowledge is missing *)
 
 (*adds further background knowledge to TBox and ABox*)
@@ -99,74 +107,74 @@ axiomatization where
   A6: "GeorgeOrwell : British" (*is he?*)
 (* .... *)
 
-(*Now we get what we intended*)
+(*Now we can in fact infer that GeorgeOrwell is European (given the background information)*)
 lemma "GeorgeOrwell : European" (*is he?*)
   by (meson A6 T3 subset_def)
 
 
 
-subsection \<open>Extended language\<close>
+subsection \<open>Extended description language\<close>
 
 
-(*Nominals*)
+(*HOL-variables can act as nominals (i.e. proper names for individuals)*)
 lemma "x : \<^bold>\<exists>bornIn\<sqdot>{Bamberg} \<leftrightarrow> bornIn x Bamberg"
   by (simp add: rPreimage_def)
 lemma "Anne : \<^bold>\<exists>bornIn\<sqdot>{Tokio, London, Bamberg} \<leftrightarrow> (bornIn Anne Tokio) \<or> (bornIn Anne London) \<or> (bornIn Anne Bamberg)"
   by (smt (verit) rPreimage_def)
 
 
-(*Role composition*)
-abbreviation dl_role_comp::"ERel('i) \<Rightarrow> ERel('i) \<Rightarrow> ERel('i)" (infixr "\<sqdot>" 90)
+(*Roles (being relations) can be composed with each other (cf. relation composition)*)
+abbreviation role_composition::"ERel('i) \<Rightarrow> ERel('i) \<Rightarrow> ERel('i)" (infixr "\<sqdot>" 90)
   where "r\<^sub>1 \<sqdot> r\<^sub>2 \<equiv> r\<^sub>2 \<circ>\<^sup>r r\<^sub>1"
 
 (*Range restriction (for roles)*)
-abbreviation range_restr::"ERel('i) \<Rightarrow> Set('i) \<Rightarrow> ERel('i)" ("_|_")
+abbreviation range_restriction::"ERel('i) \<Rightarrow> Set('i) \<Rightarrow> ERel('i)" ("_|_")
   where "R|C \<equiv> C\<downharpoonright>R"
 
 (*useful derived definition (cf. dynamic logic)*)
-definition test::"Set('i) \<Rightarrow> ERel('i)" ("_?")
+abbreviation test::"Set('i) \<Rightarrow> ERel('i)" ("_?")
   where "P? \<equiv> (=)|P"
 
 (*Both notions are interrelated*)
 lemma "R|C = (R \<sqdot> C?)"
-  unfolding rcomp_def test_def by auto
+  unfolding rcomp_def by auto
 
 
 (*An example:*)
 consts consumes::r
 consts Drink::c
-definition drinks::r
+abbreviation drinks::r
   where "drinks x d \<equiv> consumes x d \<and> Drink d"
 
 lemma "drinks = (consumes|Drink)"
-  unfolding rcomp_def drinks_def by auto
+  unfolding rcomp_def by auto
 
 lemma "drinks \<subseteq>\<^sup>r consumes"
-  by (simp add: drinks_def subset_def)
+  by (simp add: subset_def)
 
-
-(*****Miscellaneous*****)
 
 (*Transitive closure*)
-definition tran_closure::"ERel('i) \<Rightarrow> ERel('i)" ("_\<^sup>+")
+abbreviation tran_closure::"ERel('i) \<Rightarrow> ERel('i)" ("_\<^sup>+")
   where \<open>r\<^sup>+ \<equiv> \<Inter>\<^sup>r(\<lambda>\<rho>. r \<subseteq>\<^sup>r \<rho> \<and> transitive \<rho>)\<close>
 
 (*A typical example for transitive closure*)
-definition \<open>ancestorOf \<equiv> parentOf\<^sup>+\<close>
+abbreviation \<open>ancestorOf \<equiv> parentOf\<^sup>+\<close>
 
-term "Finite_Set"
 
 (*Another example, involving dating*)
+
 lemma assumes "symmetric(dates)"
           and "Klaus \<noteq> John \<and> Klaus \<noteq> Mary"
           and "Anne : \<^bold>\<forall>dates\<sqdot>{John,Mary}"
-        shows "Klaus : \<^bold>\<exists>dates\<^sup>-\<sqdot>{Anne}"
+        shows "Klaus : \<^bold>\<exists>(\<midarrow>\<^sup>rdates)\<sqdot>{Anne}"
   by (smt (verit, ccfv_SIG) Symmetric_def assms(1) assms(2) assms(3) compl_def rDualPreimage_def rPreimage_def)
 
 
-(****************************************************************************)
-(* Exercise: defining (qualified) number restrictions \<le>\<^sub>nr (\<le>\<^sub>nr.C)           *)
-(****************************************************************************)
+
+
+subsection \<open>Example 2 - defining (qualified) number restrictions\<close>
+
+(*Number restrictions are expressions of the form:  \<le>\<^sub>nr (\<le>\<^sub>nr.C) *)
 
 (* Useful(?) definitions*)
 abbreviation diff_dia::"Set('i) \<Rightarrow> Set('i)" ("\<D>")
